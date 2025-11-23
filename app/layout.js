bff3063963acf60cf60e78399c17fcf8b2e8596e
Anvy1311 <anvymaheshwari@gmail.com>
2025-11-23 18:26:54 +0530

    Please hojaa

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,48 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { dark } from "@clerk/themes";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({
-  subsets: ["latin"]
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "MedCare - Doctor Appointment App",
-  description: "Book and manage your doctor appointments with ease using MedCare.",
+  title: "Doctors Appointment App",
+  description: "Connect with doctors anytime, anywhere",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.classname}`}
-      >
-        {/* header */}
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
 
-        <main className="min-h-screen">{children} </main>
-
-        {/* footer */}
-
-        <footer className="bg-muted/50 py-12">
-          <div children="container mx-auto px-4 text-center text-gray-200">
-            <p>© 2024 MedCare. All rights reserved.</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with 💗 by RoadsideCoder</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
